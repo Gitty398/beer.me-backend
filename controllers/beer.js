7609b5359aa17bcc70d7cc955d17dbafeb2ccdac
Thomas Phillips <fatesunited@gmail.com>
2026-02-09 11:58:19 -0600

    added in delete and update routes for backend

--- controllers/beer.js
+++ controllers/beer.js
@@ -74,10 +74,71 @@ router.get('/:beerId',  async (req, res) => {
   }
 });
 
+// Delete
 
+router.delete('/:beerId', async (req, res) => {
+  try {
+    const beerToDelete = await Beer.findById(req.params.beerId);
+    if (!beerToDelete) {
+      res.status(404);
+      throw new Error("Could not find beer to delete");
+    }
+    if (!beerToDelete.owner.equals(req.user._id)) {
+      res.status(403);
+      throw new Error("You are not authorized to delete this beer");
+    }
 
+    await beerToDelete.deleteOne();
 
+    res.status(200).json(beerToDelete);
+  } catch (error) {
+    const { statusCode } = res;
+    res
+      .status([403, 404].includes(statusCode) ? statusCode : 500)
+      .json({ err: error.message });
+  }
+});
 
+// Update
 
+router.put('/:beerId', async (req, res) => {
+  try {
+    const foundBeer = await Beer.findById(req.params.beerId);
+    if (!foundBeer.owner.equals(req.user._id)) {
+      res.status(403);
+      throw new Error(`You can only edit beers you own`);
+    }
+
+    if (!CATEGORIES.includes(req.body.category)) {
+      throw new Error(
+        `${req.body.category} is not a valid category. Please provide one of: ${CATEGORIES.join(", ")}`,
+      );
+    }
+
+    // if (!req.body.text.trim() || !req.body.title.trim()) {
+    //   throw new Error(`The body and title fields must have valid text`);
+    // }
+
+    const updatedBeer = await Beer.findByIdAndUpdate(
+      req.params.beerId,
+      req.body,
+      { new: true },
+    );
+
+    if (!updatedBeer) {
+      throw new Error("Failed to updated beer. Please try again");
+    }
+
+    updatedBeer._doc.owner = req.user;
+
+    res.status(200).json(updatedBeer);
+  } catch (error) {
+    if (res.statusCode === 403) {
+      res.json({ err: error.message });
+    } else {
+      res.status(500).json({ err: error.message });
+    }
+  }
+});
 
 module.exports = router;
